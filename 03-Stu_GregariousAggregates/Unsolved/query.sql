c934d061114931c9b8ff40abc3a2daef142f88a6
David Gayman <drg.developer.official@gmail.com>
2021-10-10 19:05:39 +0100

    Added queries.

--- 03-Stu_GregariousAggregates/Unsolved/query.sql
+++ 03-Stu_GregariousAggregates/Unsolved/query.sql
@@ -1,17 +1,45 @@
+-- Delete and make the payments table
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments (
+	payment_id SERIAL PRIMARY KEY,
+	customer_id INT,
+	staff_id INT,
+	rental_id INT,
+	amount NUMERIC(5,2),
+	payment_date TIMESTAMP
+);
+
+SELECT * FROM payments;
+
 --1. What is the average payment amount?
 
+SELECT AVG(amount) AS "avg_payment_amount"
+from payments;
 
 --2. What is the total payment amount?
 
+SELECT SUM(amount) AS "total_payment_amount"
+from payments;
 
 --3. What is the minimum payment amount?
 
+SELECT MIN(amount) AS "min_payment_amount"
+from payments;
 
 --4. What is the maximum payment amount?
 
+SELECT MAX(amount) AS "max_payment_amount"
+from payments;
 
 --5. What is the count of payments for each customer?
 
+SELECT customer_id, COUNT(*) AS "payment_count"
+FROM payments
+GROUP BY customer_id;
 
 --6. How many customers has each staff serviced?
 
+SELECT staff_id, COUNT(*) AS "number_of_staff_serviced"
+FROM payments
+GROUP BY staff_id;
--- 05-Stu_Order_By/Unsolved/query.sql
+++ 05-Stu_Order_By/Unsolved/query.sql
@@ -1,21 +1,57 @@
 -- Find the count of payments per customer in descending order
 
+SELECT customer_id, COUNT(*) AS "payment_count"
+FROM payments
+GROUP BY customer_id 
+ORDER BY payment_count DESC;
 
 -- Find the top 5 customers who have spend the most money
 
+SELECT customer_id, SUM(amount) AS "payment_total"
+FROM payments
+GROUP BY customer_id
+ORDER BY payment_total DESC
+LIMIT 5;
 
 -- Find the bottom 5 customers who have spend the least money
 
+SELECT customer_id, SUM(amount) AS "payment_total"
+FROM payments
+GROUP BY customer_id
+ORDER BY payment_total ASC
+LIMIT 5;
 
 -- Find the top 10 customers with the highest average payment
--- rounded to two decimal places
 
+SELECT customer_id, AVG(amount) AS "payment_average"
+FROM payments
+GROUP BY customer_id
+ORDER BY payment_average DESC
+LIMIT 10;
+
+-- rounded to two decimal places
+-- TODO ROUND(,2) not working
+
+SELECT * FROM payments;
+
+SELECT customer_id, ROUND(AVG(amount), 2) AS "payment_average"
+FROM payments
+GROUP BY customer_id
+ORDER BY payment_average DESC
+LIMIT 10;
 
 -- BONUS 1
 
+SELECT * FROM customer;
 
+-- SELECT staff_id, COUNT(*) AS "number_of_customers_serviced"
+-- FROM payments
+-- GROUP BY staff_id
+-- ORDER BY number_of_customers_serviced DESC;
 
 -- BONUS 2
 
-
+-- SELECT staff_id, COUNT(customer_id) AS "customer_count"
+-- FROM payments
+-- GROUP BY staff_id;
 
